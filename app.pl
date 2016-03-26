89c8a21b40f8938007e76e063dd64e26c1cc7c97
Carl Masak <cmasak@gmail.com>
2016-03-26 22:51:56 +0100

    flesh app page out a bit

--- app.pl
+++ app.pl
@@ -1,5 +1,43 @@
 use Bailador;
+
 get '/' => sub {
-    "hello world"
+    return q:to'HTML';
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <title>Nex</title>
+        </head>
+        <body>
+            <pre><code>. . . . . . . . . . . . .
+         . . . . . . . . . . . . .
+          . . . . . . . . . . . . .
+           . . . . . . . . . . . . .
+            . . . . . . . . . . . . .
+             . . . . . . . . . . . . .
+              . . . . . . . . . . . . .
+               . . . . . . . . . . . . .
+                . . . . . . . . . . . . .
+                 . . . . . . . . . . . . .
+                  . . . . . . . . . . . . .
+                   . . . . . . . . . . . . .
+                    . . . . . . . . . . . . .</code></pre>
+
+            <form action="//game" method="post">
+                <label for="placement-player">Player: </label>
+                    <input type="text" id="placement-player"><br>
+                <label for="placement-own-stone-row">Own stone row: </label>
+                    <input type="text" id="placement-own-stone-row"><br>
+                <label for="placement-own-stone-column">Own stone column: </label>
+                    <input type="text" id="placement-own-stone-column"><br>
+                <label for="placement-own-stone-row">Own stone row: </label>
+                    <input type="text" id="placement-own-stone-row"><br>
+                <label for="placement-own-stone-column">Own stone column: </label>
+                    <input type="text" id="placement-own-stone-column"><br>
+                <input type="submit" value="Send">
+            </form>
+        </body>
+        HTML
 }
+
 baile( Int(%*ENV<PORT> || 5000) );
